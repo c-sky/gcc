fb9266227aa2d6d2006bda5a2cb07b0023378c0d
jojo <jiejie_rong@c-sky.com>
2017-02-08 13:07:11 +0800

    GCC-66:abiv2: add base build info of csky
    Change-Id: Ide766665d6bdfebe4d76dd8d35c9622ebfb865da

--- gcc/config/csky/abiv2_csky_internal.h
+++ gcc/config/csky/abiv2_csky_internal.h
@@ -5,30 +5,7 @@
 
 #include "sbitmap.h"
 #include "abiv2_csky_isa.h"
-
-/* The various CSKY cores.  */
-enum csky_processor_type
-{
-#undef CSKY_CORE
-#define CSKY_CORE(NAME, INTERNAL_IDENT, IDENT, ARCH, ISA) \
-  TARGET_CPU_##INTERNAL_IDENT,
-#include "abiv2_csky_cores.def"
-#undef CSKY_CORE
-  /* Used to indicate that no processor has been specified.  */
-  TARGET_CPU_csky_none
-};
-
-/* The various CSKY architectures.  */
-enum csky_base_architecture
-{
-  CSKY_BASE_ARCH_NONE,
-#undef CSKY_ARCH
-#define CSKY_ARCH(NAME, CORE_IDENT, ARCH, ISA) \
-  CSKY_BASE_ARCH_##ARCH,
-#include "abiv2_csky_arches.def"
-#undef CSKY_ARCH
-   CSKY_BASE_ARCH_MAX
-};
+#include "abiv2_csky_opts.h"
 
 struct csky_processors
 {
@@ -37,16 +14,6 @@ struct csky_processors
   const char *arch;
   enum csky_base_architecture base_arch;
   enum csky_isa_feature isa_bits[CSKY_ISA_FEATURE_GET(max)];
-};
-
-/* The various CSKY FPUs.  */
-enum csky_fpu_type
-{
-#undef CSKY_FPU
-#define CSKY_FPU(NAME, CNAME, ISA) TARGET_FPU_##CNAME,
-#include "abiv2_csky_fpus.def"
-  TARGET_FPU_auto
-#undef CSKY_FPU
 };
 
 struct csky_fpu_desc
---
+++ gcc/config/csky/abiv2_csky_opts.h
@@ -0,0 +1,41 @@
+
+#ifndef CSKY_OPTS_H
+#define CSKY_OPTS_H
+
+
+/* The various CSKY cores.  */
+enum csky_processor_type
+{
+#undef CSKY_CORE
+#define CSKY_CORE(NAME, INTERNAL_IDENT, IDENT, ARCH, ISA) \
+  TARGET_CPU_##INTERNAL_IDENT,
+#include "abiv2_csky_cores.def"
+#undef CSKY_CORE
+  /* Used to indicate that no processor has been specified.  */
+  TARGET_CPU_csky_none
+};
+
+/* The various CSKY architectures.  */
+enum csky_base_architecture
+{
+  CSKY_BASE_ARCH_NONE,
+#undef CSKY_ARCH
+#define CSKY_ARCH(NAME, CORE_IDENT, ARCH, ISA) \
+  CSKY_BASE_ARCH_##ARCH,
+#include "abiv2_csky_arches.def"
+#undef CSKY_ARCH
+   CSKY_BASE_ARCH_MAX
+};
+
+/* The various CSKY FPUs.  */
+enum csky_fpu_type
+{
+#undef CSKY_FPU
+#define CSKY_FPU(NAME, CNAME, ISA) TARGET_FPU_##CNAME,
+#include "abiv2_csky_fpus.def"
+  TARGET_FPU_auto
+#undef CSKY_FPU
+};
+
+
+#endif /* CSKY_OPTS_H */
