87a6e7c2c83333a51fd9cf7d9a73953f405c7ea9
quxm <xianmiao_qu@c-sky.com>
2017-04-12 15:17:10 +0800

    GCC-227:Pass export-dynamic to linker only when -static is not added.
    Change-Id: I6a228c8b17aacdc7ea98da0fffefdb6a0981efdf

--- gcc/config/csky/csky-abiv2-linux-elf.h
+++ gcc/config/csky/csky-abiv2-linux-elf.h
@@ -38,8 +38,9 @@
    %{static:-Bstatic}                                           \
    %{shared:-shared}                                            \
    %{symbolic:-Bsymbolic}                                       \
-   %{rdynamic:-export-dynamic}                                  \
-   %{!dynamic-linker:-dynamic-linker " LINUX_DYNAMIC_LINKER "}  \
+   %{!static:                                                   \
+     %{rdynamic:-export-dynamic}                                \
+     %{!shared:-dynamic-linker " LINUX_DYNAMIC_LINKER "}}       \
    -X                                                           \
    %{mbig-endian:-EB} %{mlittle-endian:-EL}                     \
    %{EB:-EB} %{EL:-EL}"
