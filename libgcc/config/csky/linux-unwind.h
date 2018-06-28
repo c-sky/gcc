741c333bd165fc011e917cc485979ad03951d8d9
quxm <xianmiao_qu@c-sky.com>
2018-06-28 10:58:53 +0800

    Modify linux-unwind.h
    Change-Id: Id8fbed1f1641630b41f1a5a5a534dabf62f51c76

--- libgcc/config/csky/linux-unwind.h
+++ libgcc/config/csky/linux-unwind.h
@@ -1,5 +1,5 @@
-/* DWARF2 EH unwinding support for MIPS Linux.
-   Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+/* DWARF2 EH unwinding support for C-SKY Linux.
+   Copyright (C) 2018 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -26,6 +26,16 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 /* Do code reading to identify a signal frame, and set the frame
    state data appropriately.  See unwind-dw2.c for the structs.  */
 
+#include <linux/version.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0))
+#define sc_pt_regs(x) (sc->sc_##x)
+#define sc_pt_regs_tls(x) (sc->sc_exregs[15])
+#else
+#define sc_pt_regs(x) (sc->sc_pt_regs.x)
+#define sc_pt_regs_tls(x) sc_pt_regs(x)
+#endif
+
 #include <signal.h>
 #include <asm/unistd.h>
 
@@ -50,9 +60,11 @@ csky_fallback_frame_state (struct _Unwind_Context *context,
   struct sigcontext *sc;
   _Unwind_Ptr new_cfa;
   int i;
-  // FIXME
-  /* movi r1, __NR_rt_sigreturn; trap 0  */
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0))
+/* #define _NR_sigreturn 119 */
 #ifdef  __CSKYABIV1__
+  /* movi r1, __NR_sigreturn; trap 0  */
   if ((*(pc+0) == (0x6000 + (119 << 4) + 1)) && (*(pc+1) == 0x0008))
 #else
   /* movi r7, __NR_rt_sigreturn; trap 0  */
@@ -69,8 +81,10 @@ csky_fallback_frame_state (struct _Unwind_Context *context,
     } *_rt = context->cfa;
     sc = _rt->psc; // &(_rt->sc);
   }
-  /* movi r1, 127, addi r1, 32, addi r1, (_NR_rt_sigreturn - 127 - 32); trap 0  */
+
+/* #define _NR_rt_sigreturn 173 */
 #ifdef  __CSKYABIV1__
+  /* movi r1, 127, addi r1, 32, addi r1, (_NR_rt_sigreturn - 127 - 32); trap 0  */
   else if((*(pc+0) == (0x6000 + (127 << 4) + 1)) && (*(pc+1) == (0x2000 + (31 << 4) + 1)) &&
           (*(pc+2) == (0x2000 + ((173 - 127 - 32 - 1) << 4) + 1)) && (*(pc+3) == 0x0008))
 #else
@@ -78,19 +92,39 @@ csky_fallback_frame_state (struct _Unwind_Context *context,
   else if ((*(pc+0) == 0xea07) && (*(pc+1) == 173) &&
       (*(pc+2) == 0xc000) &&  (*(pc+3) == 0x2020))
 #endif
+#else
+/* #define _NR_rt_sigreturn 139 */
+#ifdef  __CSKYABIV1__
+ /*
+  * movi r1, 127
+  * addi r1, (_NR_rt_sigreturn - 127)
+  * trap 0
+  */
+  if((*(pc+0) == (0x6000 + (127 << 4) + 1)) &&
+     (*(pc+1) == (0x2000 + ( 12 << 4) + 1)) &&
+     (*(pc+2) ==  0x0008))
+#else
+  /*
+   * movi r7, __NR_rt_sigreturn
+   * trap 0
+   */
+  if ((*(pc+0) == 0xea07) && (*(pc+1) == 139) &&
+      (*(pc+2) == 0xc000) && (*(pc+3) == 0x2020))
+#endif
+#endif
   {
     struct rt_sigframe {
       int sig;
       struct siginfo *pinfo;
       void* puc;
       siginfo_t info;
-      struct ucontext uc;
+      ucontext_t uc;
     } *_rt = context->cfa;
     sc = &(_rt->uc.uc_mcontext);
   }
   else  return _URC_END_OF_STACK;
 
-  new_cfa = (_Unwind_Ptr) sc->sc_usp;
+  new_cfa = (_Unwind_Ptr) sc->sc_pt_regs.usp;
   fs->regs.cfa_how = CFA_REG_OFFSET;
   fs->regs.cfa_reg = STACK_POINTER_REGNUM;
   fs->regs.cfa_offset = new_cfa - (_Unwind_Ptr) context->cfa;
@@ -102,48 +136,49 @@ csky_fallback_frame_state (struct _Unwind_Context *context,
 #define ARG_OFF 0
 #endif
   fs->regs.reg[0 + ARG_OFF].how = REG_SAVED_OFFSET;
-  fs->regs.reg[0 + ARG_OFF].loc.offset = (_Unwind_Ptr)&(sc->sc_a0) - new_cfa;
+  fs->regs.reg[0 + ARG_OFF].loc.offset = (_Unwind_Ptr)&sc_pt_regs(a0) - new_cfa;
 
   fs->regs.reg[1 + ARG_OFF].how = REG_SAVED_OFFSET;
-  fs->regs.reg[1 + ARG_OFF].loc.offset = (_Unwind_Ptr)&(sc->sc_a1) - new_cfa;
+  fs->regs.reg[1 + ARG_OFF].loc.offset = (_Unwind_Ptr)&sc_pt_regs(a1) - new_cfa;
 
   fs->regs.reg[2 + ARG_OFF].how = REG_SAVED_OFFSET;
-  fs->regs.reg[2 + ARG_OFF].loc.offset = (_Unwind_Ptr)&(sc->sc_a2) - new_cfa;
+  fs->regs.reg[2 + ARG_OFF].loc.offset = (_Unwind_Ptr)&sc_pt_regs(a2) - new_cfa;
 
   fs->regs.reg[3 + ARG_OFF].how = REG_SAVED_OFFSET;
-  fs->regs.reg[3 + ARG_OFF].loc.offset = (_Unwind_Ptr)&(sc->sc_a3) - new_cfa;
+  fs->regs.reg[3 + ARG_OFF].loc.offset = (_Unwind_Ptr)&sc_pt_regs(a3) - new_cfa;
 
 #ifdef  __CSKYABIV1__
   for (i = 6; i < 15; i++) {  /* for abiv1, r6~r14 */
     fs->regs.reg[i].how = REG_SAVED_OFFSET;
-    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&(sc->sc_regs[i - 6]) - new_cfa;
+    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&sc_pt_regs(regs[i - 6]) - new_cfa;
   }
   /* for abiv1, r1 */
   fs->regs.reg[1].how = REG_SAVED_OFFSET;
-  fs->regs.reg[1].loc.offset = (_Unwind_Ptr)&(sc->sc_regs[9]) - new_cfa;
+  fs->regs.reg[1].loc.offset = (_Unwind_Ptr)&sc_pt_regs(regs[9]) - new_cfa;
 #else
   for (i = 4; i < 14; i++) {  /* for abiv2, r4~r13 */
     fs->regs.reg[i].how = REG_SAVED_OFFSET;
-    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&(sc->sc_regs[i - 4]) - new_cfa;
+    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&sc_pt_regs(regs[i - 4]) - new_cfa;
   }
 
-  for (i = 16; i < 32; i++) {  /* for abiv2, r16~r31 */
+  for (i = 16; i < 31; i++) {  /* for abiv2, r16~r30 */
     fs->regs.reg[i].how = REG_SAVED_OFFSET;
-    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&(sc->sc_exregs[i - 16]) - new_cfa;
+    fs->regs.reg[i].loc.offset = (_Unwind_Ptr)&sc_pt_regs(exregs[i - 16]) - new_cfa;
   }
 
+    fs->regs.reg[31].loc.offset = (_Unwind_Ptr)&sc_pt_regs_tls(tls) - new_cfa;
   /* FIXME : hi lo ? */
 #endif
   fs->regs.reg[15].how = REG_SAVED_OFFSET;
-  fs->regs.reg[15].loc.offset = (_Unwind_Ptr)&(sc->sc_r15) - new_cfa;
+  fs->regs.reg[15].loc.offset = (_Unwind_Ptr)&sc_pt_regs(lr) - new_cfa;
 
   fs->regs.reg[56].how = REG_SAVED_OFFSET;
-  fs->regs.reg[56].loc.offset = (_Unwind_Ptr)&(sc->sc_pc) - new_cfa;
+  fs->regs.reg[56].loc.offset = (_Unwind_Ptr)&sc_pt_regs(pc) - new_cfa;
   fs->retaddr_column = 56;
   fs->signal_frame = 1;
 
   return _URC_NO_REASON;
 }
 
-
 #endif
+
